bdf9a9b48f475760b5fd0e64d7dc74fed8759c32
ostro3ve3d <ostro3ve3d2@gmail.com>
2025-01-28 23:42:08 +0400

    Add initialization of necessary objects and creation of configs when loading the plugin

--- src/main/java/ru/bambolumba/durabilitynotifier/ProjectDurability.java
+++ src/main/java/ru/bambolumba/durabilitynotifier/ProjectDurability.java
@@ -1,12 +1,25 @@
 package ru.bambolumba.durabilitynotifier;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import ru.bambolumba.durabilitynotifier.Listeners.BlockBreakListener;
+import ru.bambolumba.durabilitynotifier.Notifications.ActionBarType;
+import ru.bambolumba.durabilitynotifier.Notifications.MessageType;
+import ru.bambolumba.durabilitynotifier.Utils.ConfigManager;
 
 public final class ProjectDurability extends JavaPlugin {
 
+    private MessageType messageType;
+    private ActionBarType actionBarType;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+
+        messageType = new MessageType();
+        actionBarType = new ActionBarType();
+
+        ConfigManager.createConfigFiles();
+
+        getServer().getPluginManager().registerEvents(new BlockBreakListener(), this);
 
     }
 
@@ -14,4 +27,13 @@ public final class ProjectDurability extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+    public MessageType getMessage() {
+        return messageType;
+    }
+
+    public ActionBarType getActionBar() {
+        return actionBarType;
+    }
+
 }
