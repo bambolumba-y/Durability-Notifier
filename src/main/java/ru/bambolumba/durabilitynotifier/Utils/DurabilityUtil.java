57da8b0628ae456fbca734da4c1a4d1e14e44d98
ostro3ve3d <ostro3ve3d2@gmail.com>
2025-01-28 23:40:16 +0400

    Add util to work with durability

---
+++ src/main/java/ru/bambolumba/durabilitynotifier/Utils/DurabilityUtil.java
@@ -0,0 +1,16 @@
+package ru.bambolumba.durabilitynotifier.Utils;
+
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
+
+public class DurabilityUtil {
+
+    public static int getDurability(ItemStack itemStack, Damageable damageable) {
+        return itemStack.getType().getMaxDurability() - damageable.getDamage();
+    }
+
+    public static boolean isDurabilityLow(int durability) {
+        return durability < ConfigManager.getConfig().getInt("notifications.required-percentage");
+    }
+
+}
