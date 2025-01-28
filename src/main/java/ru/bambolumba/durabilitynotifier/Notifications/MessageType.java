fffb4fbca69fefd7201b3b8f650ef0d819b811ae
ostro3ve3d <ostro3ve3d2@gmail.com>
2025-01-28 23:40:52 +0400

    Renamed for convenience

--- src/main/java/ru/bambolumba/durabilitynotifier/Notifications/MessageType.java
+++ src/main/java/ru/bambolumba/durabilitynotifier/Notifications/MessageType.java
@@ -2,12 +2,12 @@ package ru.bambolumba.durabilitynotifier.Notifications;
 
 import ru.bambolumba.durabilitynotifier.Utils.ConfigManager;
 
-public class Message {
+public class MessageType {
 
-    private boolean enabled;
-    private String text;
+    private final boolean enabled;
+    private final String text;
 
-    public Message () {
+    public MessageType() {
         this.enabled = ConfigManager.getConfig().getBoolean("notifications.types.message.enabled");
         this.text = ConfigManager.getMessageConfig().getString("notifications.types.message.text");
     }
