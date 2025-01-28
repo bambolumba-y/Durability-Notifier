550f154b21cb32f3fcca908093b831e3862ca51c
ostro3ve3d <ostro3ve3d2@gmail.com>
2025-01-28 23:40:57 +0400

    Renamed for convenience

--- src/main/java/ru/bambolumba/durabilitynotifier/Utils/MessageUtil.java
+++ src/main/java/ru/bambolumba/durabilitynotifier/Utils/MessageUtil.java
@@ -6,15 +6,15 @@ import net.kyori.adventure.text.minimessage.MiniMessage;
 
 import static ru.bambolumba.durabilitynotifier.Utils.ConfigManager.getMessageConfig;
 
-public class PrepareMessage {
+public class MessageUtil {
 
-    public static Component buildMessage(String configPath) {
+    public static Component build(String configPath) {
         String message = getMessageConfig().getString(configPath);
         MiniMessage miniMessage = MiniMessage.miniMessage();
         return miniMessage.deserialize(message);
     }
 
-    public static Component buildMessage(String configPath, String match, String replacement) {
+    public static Component build(String configPath, String match, String replacement) {
         String message = getMessageConfig().getString(configPath);
         MiniMessage miniMessage = MiniMessage.miniMessage();
         return miniMessage.deserialize(message).replaceText(TextReplacementConfig.builder()
@@ -23,12 +23,12 @@ public class PrepareMessage {
                 .build());
     }
 
-    public static Component buildMessage(String message, boolean isFinal) {
+    public static Component build(String message, boolean isFinal) {
         MiniMessage miniMessage = MiniMessage.miniMessage();
         return miniMessage.deserialize(message);
     }
 
-    public static Component buildMessage(String message, String messageToAdd) {
+    public static Component build(String message, String messageToAdd) {
         String finalMessage = message + messageToAdd;
         MiniMessage miniMessage = MiniMessage.miniMessage();
         return miniMessage.deserialize(finalMessage);
