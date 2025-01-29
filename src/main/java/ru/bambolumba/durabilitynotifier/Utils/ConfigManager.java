a24e5ec25e219deedd62ed9589156ff450444697
ostro3ve3d <ostro3ve3d2@gmail.com>
2025-01-29 17:59:39 +0400

    Methods that duplicate methods of another class have been removed

--- src/main/java/ru/bambolumba/durabilitynotifier/Utils/ConfigManager.java
+++ src/main/java/ru/bambolumba/durabilitynotifier/Utils/ConfigManager.java
@@ -41,32 +41,6 @@ public class ConfigManager {
 
     }
 
-    public static Component buildMessage(String configPath) {
-        String message = getMessageConfig().getString(configPath);
-        MiniMessage miniMessage = MiniMessage.miniMessage();
-        return miniMessage.deserialize(message);
-    }
-
-    public static Component buildMessage(String configPath, String match, String replacement) {
-        String message = getMessageConfig().getString(configPath);
-        MiniMessage miniMessage = MiniMessage.miniMessage();
-        return miniMessage.deserialize(message).replaceText(TextReplacementConfig.builder()
-                .match(match)
-                .replacement(replacement)
-                .build());
-    }
-
-    public static Component buildMessage(String message, boolean isFinal) {
-        MiniMessage miniMessage = MiniMessage.miniMessage();
-        return miniMessage.deserialize(message);
-    }
-
-    public static Component buildMessage(String message, String messageToAdd) {
-        String finalMessage = message + messageToAdd;
-        MiniMessage miniMessage = MiniMessage.miniMessage();
-        return miniMessage.deserialize(finalMessage);
-    }
-
     public static void reloadConfigFiles() {
         plugin.reloadConfig();
         createMessageConfig();
