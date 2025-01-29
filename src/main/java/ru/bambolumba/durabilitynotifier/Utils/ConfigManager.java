223eba47acb32f7e9e6c105182e99cd468e4d1b6
ostro3ve3d <ostro3ve3d2@gmail.com>
2025-01-30 01:04:20 +0400

    Add method to obtain sounds from config

--- src/main/java/ru/bambolumba/durabilitynotifier/Utils/ConfigManager.java
+++ src/main/java/ru/bambolumba/durabilitynotifier/Utils/ConfigManager.java
@@ -1,10 +1,16 @@
 package ru.bambolumba.durabilitynotifier.Utils;
 
+import org.bukkit.NamespacedKey;
+import org.bukkit.Registry;
+import org.bukkit.Sound;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import ru.bambolumba.durabilitynotifier.DurabilityNotifier;
 
 import java.io.File;
+
+import static org.bukkit.Bukkit.getLogger;
+import static org.bukkit.Effect.Type.SOUND;
 
 public class ConfigManager {
 
@@ -42,6 +48,25 @@ public class ConfigManager {
         plugin.reloadConfig();
         createMessageConfig();
         plugin.applyNotificationsUpdate();
+    }
+
+    /*
+    Sound should be in format minecraft:example.example.example. If there would be _ instead of . , so this method will return null
+     */
+    public static Sound getSoundFromConfig(String path, Sound defaultSound) {
+        String soundName = getConfig().getString(path);
+        if (soundName != null) {
+            soundName = "minecraft:" + soundName;
+            try {
+                NamespacedKey soundKey = NamespacedKey.fromString(soundName.toLowerCase().replace("_", "."));
+                if (soundKey != null) {
+                    Sound sound = Registry.SOUNDS.get(soundKey);
+                    return sound;
+                }
+            } catch (IllegalArgumentException e) {
+            }
+        }
+        return defaultSound;
     }
 
 }
